14f5a5c5f778a0eb97d66ad8d280d4889025eab1
stg-annon <14135675+stg-annon@users.noreply.github.com>
2021-09-17 17:44:11 -0400

    refactor item updates to parser module

--- bulk_scrape.py
+++ bulk_scrape.py
@@ -1,4 +1,5 @@
 import json
+from scrape_parser import ScrapeParser
 import sys
 import time
 import datetime
@@ -62,6 +63,7 @@ class ScrapeController:
 			log.warning("Using defaults for wrong values")
 
 		self.client = client
+		self.parse = ScrapeParser(client)
 
 		self.client.reload_scrapers()
 
@@ -417,110 +419,33 @@ class ScrapeController:
 			self.client.scrape_scene_url,
 			self.__update_scene_with_scrape_data
 		)
-	def __update_scene_with_scrape_data(self, scene, scraped_data):
+	def __update_scene_with_scrape_data(self, scene, scraped_scene):
 		
 		update_data = {
 			'id': scene.get('id')
 		}
 
 		common_attrabutes = ['url','title','details','date']
-		for c_attr in common_attrabutes:
-			if scraped_data.get(c_attr):
-				update_data[c_attr] = scraped_data.get(c_attr)
+		update_data.update( self.parse.get_common_atttrs( scraped_scene, common_attrabutes ) )
 
-		if scraped_data.get('image'):
-			update_data['cover_image'] = scraped_data.get('image')
+		if scraped_scene.get('image'):
+			update_data['cover_image'] = scraped_scene.get('image')
 
-		if scraped_data.tags:
-			tag_ids = list()
-			for tag in scraped_data.tags:
-				if tag.stored_id:
-					tag_ids.append(tag.stored_id)
-				elif config.create_missing_tags and tag.name:
-					tag_name = caps_string(tag.name)
-					log.info(f'Create missing tag: {tag_name}')
-					tag_ids.append(self.client.create_tag({'name':tag_name}))
-			if len(tag_ids) > 0:
-				update_data['tag_ids'] = tag_ids
+		if scraped_scene.tags:
+			update_data['tag_ids'] = self.parse.get_tag_ids(scraped_scene.tags)
 
-		if scraped_data.performers:
-			performer_ids = list()
-			for performer in scraped_data.performers:
-				if performer.stored_id:
-					performer_ids.append(performer.stored_id)
-				elif performer.name:
-					# scraper could not match performer, try re-matching or create if enabled
-					performer.name = performer.name.strip()
-					performer.name = caps_string(performer.name)
+		if scraped_scene.performers:
+			update_data['performer_ids'] = self.parse.get_performer_ids(scraped_scene.performers)
 
-					perf_in = {'name': performer.name }
-					if performer.url:
-						perf_in['url'] = performer.url 
-						
-					stash_perf = self.client.find_performer(perf_in, create_missing=config.create_missing_performers)
-					if stash_perf and stash_perf.get("id"):
-						performer_ids.append(stash_perf.id)
-					
-			if len(performer_ids) > 0:
-				update_data['performer_ids'] = performer_ids
+		if scraped_scene.studio:
+			update_data['studio_id'] = self.parse.get_studio_id(scraped_scene.studio)
 
-		if scraped_data.studio:
-			if scraped_data.studio.stored_id:
-				update_data['studio_id'] = scraped_data.studio.stored_id
-			elif config.create_missing_studios:
-				studio = {
-					"name": caps_string(scraped_data.studio.name),
-					"url": '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(scraped_data.studio.url))
-				}
-				log.info(f'Creating missing studio {studio.get("name")}')
-				update_data['studio_id'] = self.client.create_studio(studio)
+		if scraped_scene.movies:
+			update_data['movies'] = self.parse.get_movie_ids(scraped_scene.movies)
 
-		if scraped_data.movies:
-			movie_ids = list()
-			for movie in scraped_data.movies:
-				if movie.stored_id:
-					movie_ids.append( {'movie_id':movie.stored_id, 'scene_index':None} )
-				elif config.create_missing_movies and movie.name:
-					log.info(f'Create missing movie: "{movie.name}"')
-
-					movie_data = {
-						'name': movie.name
-					}
-
-					common_attr = [
-						'aliases',
-						'duration',
-						'date',
-						'director',
-						'url',
-						'synopsis',
-						'front_image',
-						'back_image'
-					]
-
-					for attr in common_attr:
-						if movie[attr]:
-							movie_data[attr] = movie[attr]
-							
-					if update_data['studio_id']:
-						movie_data['studio_id'] = update_data['studio_id']
-
-					try:
-						movie_ids.append( {'movie_id':self.client.create_movie(movie_data), 'scene_index':None} )
-					except Exception as e:
-						log.error(f'movie update error')
-						log.error(str(e))
-
-			if len(movie_ids) > 0:
-				update_data['movies'] = movie_ids
-
-		if scraped_data.get('stash_ids'):
-			if scene.get('stash_ids'):
-				update_data['stash_ids'] = scene.get('stash_ids').extend(scraped_data.get('stash_ids'))
-			else:
-				update_data['stash_ids'] = scraped_data.get('stash_ids')
-
-		log.debug('mapped scrape data to scene fields')
+		# handle stash box ids
+		if scraped_scene.get('stash_ids'):
+			update_data['stash_ids'] = scene.get('stash_ids').extend(scraped_scene.get('stash_ids'))
 
 		# Only accept base64 images
 		if update_data.get('cover_image') and not update_data.get('cover_image').startswith("data:image"):
@@ -532,62 +457,15 @@ class ScrapeController:
 		update_data = utils.clean_dict(update_data)
 		self.client.update_scene(update_data)
 
-	def __update_scenes_with_stashbox_data(self, scenes, scraped_data, stashbox):
-
-		# will match durations -/+ this value
-		allowed_durr_diff = 25
-
-		# % of matching fingerprints required to match
-		durr_match_percnt = 0.9
-
-		# minimum number of fingerprints a scene must have to match
-		min_fingerprint_count = 5
+	def __update_scenes_with_stashbox_data(self, scenes, scraped_data, stashbox, only_stash_ids=False):
 
 		scene_update_ids = []
-
 		total = len(scenes)
-
 		for i, scene in enumerate(scenes):
-
 			# Update status bar
 			log.progress(i/total)
 
-			matches = []
-
-			for scene_data in scraped_data:
-
-				id_match = SimpleNamespace()
-
-				id_match.hash = None
-				id_match.oshash = False
-				id_match.phash = False
-				id_match.checksum = False
-				id_match.duration = 0
-				id_match.fingerprint_count = len(scene_data.get('fingerprints'))
-				id_match.data = scene_data
-
-				for fingerprint in scene_data.get('fingerprints'):
-					
-					if scene.get('checksum') == fingerprint.get('checksum'):
-						id_match.checksum = True
-						id_match.hash = "checksum"
-					if scene.get('phash') == fingerprint.get('hash'):
-						id_match.phash = True
-						id_match.hash = "phash"
-					if scene.get('oshash') == fingerprint.get('hash'):
-						id_match.oshash = True
-						id_match.hash = "oshash"
-
-					if not scene.get('file').get('duration') or not fingerprint.get('duration'):
-						continue
-
-					durr_diff = abs(scene.get('file').get('duration') - fingerprint.get('duration'))
-					if durr_diff <= allowed_durr_diff:
-						id_match.duration += 1
-
-				if (id_match.oshash or id_match.phash or id_match.checksum) and (id_match.duration / id_match.fingerprint_count >= durr_match_percnt) and (id_match.fingerprint_count >= min_fingerprint_count):
-					matches.append(id_match)
-
+			matches = self.__match_scene_with_stashbox_data(scene, scraped_data)
 
 			if len(matches) <= 0:
 				log.info(f"FAILED to find match for scene id:{scene['id']}")
@@ -614,6 +492,9 @@ class ScrapeController:
 						'stash_id': p.remote_site_id
 					}]
 
+			# TODO Immplament this option
+			if only_stash_ids:
+				pass
 
 			try:
 				self.__update_scene_with_scrape_data(scene, m.data)
@@ -622,6 +503,48 @@ class ScrapeController:
 				log.error(str(e))
 
 		return scene_update_ids
+
+	def __match_scene_with_stashbox_data(self, scene, stashbox_data):
+		matches = []
+		for scene_data in stashbox_data:
+
+			id_match = SimpleNamespace()
+
+			id_match.hash = None
+			id_match.oshash = False
+			id_match.phash = False
+			id_match.checksum = False
+			id_match.duration = 0
+			id_match.fingerprint_count = len(scene_data.get('fingerprints'))
+			id_match.data = scene_data
+
+			for fingerprint in scene_data.get('fingerprints'):
+				
+				if scene.get('checksum') == fingerprint.get('checksum'):
+					id_match.checksum = True
+					id_match.hash = "checksum"
+				if scene.get('phash') == fingerprint.get('hash'):
+					id_match.phash = True
+					id_match.hash = "phash"
+				if scene.get('oshash') == fingerprint.get('hash'):
+					id_match.oshash = True
+					id_match.hash = "oshash"
+
+				if not scene.get('file').get('duration') or not fingerprint.get('duration'):
+					continue
+
+				durr_diff = abs(scene.get('file').get('duration') - fingerprint.get('duration'))
+				if durr_diff <= config.stashbox_allowed_durr_diff:
+					id_match.duration += 1
+
+			has_hash = (id_match.oshash or id_match.phash or id_match.checksum)
+			durration_majority_match = (id_match.duration / id_match.fingerprint_count >= config.stashbox_match_percent)
+			minimum_required_fingerprints = (id_match.fingerprint_count >= config.stashbox_min_fingerprint_count)
+			
+			if has_hash and durration_majority_match and minimum_required_fingerprints:
+				matches.append(id_match)
+
+		return matches
 
 	def __scrape_galleries_with_fragment(self, galleries, scraper_id):
 		return self.__scrape_with_fragment(
@@ -638,70 +561,22 @@ class ScrapeController:
 			self.client.scrape_gallery_url,
 			self.__update_gallery_with_scrape_data
 		)
-	def __update_gallery_with_scrape_data(self, gallery, scraped):
-		# Expecting to cast ScrapedGallery to GalleryUpdateInput
-		# NOTE
-		# 	ScrapedGallery.studio: {scrapedSceneStudio} => GalleryUpdateInput.scene_ids: [ID!]
-		#   ScrapedGallery.tags: {ScrapedSceneTag} => GalleryUpdateInput.tag_ids: [ID!]
-		#   ScrapedGallery.performers: {scrapedScenePerformer} => GalleryUpdateInput.performer_ids: [ID!]
-
+	def __update_gallery_with_scrape_data(self, gallery, scraped_gallery):
 		update_data = {
 			'id': gallery.get('id')
 		}
 
-		common_attrabutes = [
-			'title',
-			'details',
-			'url',
-			'date'
-		]
-		for attr in common_attrabutes:
-			if scraped[attr]:
-				update_data[attr] = scraped[attr]
+		common_attrabutes = ['title','details','url','date']
+		update_data.update( self.parse.get_common_atttrs( scraped_gallery, common_attrabutes ) )
 
-		if scraped.tags:
-			tag_ids = list()
-			for tag in scraped.tags:
-				if tag.stored_id:
-					tag_ids.append(tag.stored_id)
-				elif config.create_missing_tags and tag.name:
-					tag_name = caps_string(tag.name)
-					log.info(f'Create missing tag: {tag_name}')
-					tag_ids.append(self.client.create_tag({'name':tag_name}))
-			if len(tag_ids) > 0:
-				update_data['tag_ids'] = tag_ids
+		if scraped_gallery.tags:
+			update_data['tag_ids'] = self.parse.get_tag_ids(scraped_gallery.tags)
 
-		if scraped.performers:
-			performer_ids = list()
-			for performer in scraped.performers:
-				if performer.stored_id:
-					performer_ids.append(performer.stored_id)
-				elif performer.name:
-					# scraper could not match performer, try re-matching or create if enabled
-					performer.name = performer.name.strip()
-					performer.name = caps_string(performer.name)
+		if scraped_gallery.performers:
+			update_data['performer_ids'] = self.parse.get_performer_ids(scraped_gallery.performers)
 
-					perf_in = {'name': performer.name }
-					if performer.url:
-						perf_in['url'] = performer.url 
-						
-					stash_perf = self.client.find_performer(perf_in, create_missing=config.create_missing_performers)
-					if stash_perf:
-						performer_ids.append(stash_perf.id)
-
-			if len(performer_ids) > 0:
-				update_data['performer_ids'] = performer_ids
-
-		if scraped.studio:
-			if scraped.studio.stored_id:
-				update_data['studio_id'] = scraped.studio.stored_id
-			elif config.create_missing_studios:
-				studio = {
-					'name': caps_string(scraped.studio.name),
-					'url': '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(scraped.studio.url))
-				}
-				log.info(f'Creating missing studio {studio.get("name")}')
-				update_data['studio_id'] = self.client.create_studio(studio)
+		if scraped_gallery.studio:
+			update_data['studio_id'] = self.parse.get_studio_id(scraped_gallery.studio)
 
 		gallery_tag_ids = [t.id for t in gallery.tags]
 		update_data['tag_ids'] = self.__merge_tags(gallery_tag_ids, update_data.get('tag_ids'))
@@ -716,45 +591,10 @@ class ScrapeController:
 			self.client.scrape_movie_url,
 			self.__update_movie_with_scrape_data
 		)
-	def __update_movie_with_scrape_data(self, movie, scraped_data):
-
-		# Expecting to cast ScrapedMovie to MovieUpdateInput
-		# NOTE
-		# 	ScrapedMovie.duration: String (HH:MM:SS) => MovieUpdateInput.duration: Int (Total Seconds)
-		# 	ScrapedMovie.studio: {ScrapedMovieStudio} => MovieUpdateInput.studio_id: ID
-
-		movie_update_input = {
-			'id': movie.id
-		}
-		common_attrabutes = [
-			'name',
-			'aliases',
-			'date',
-			'rating',
-			'director',
-			'url',
-			'synopsis',
-			'front_image',
-			'back_image'
-		]
-		for attr in common_attrabutes:
-			if scraped_data[attr]:
-				movie_update_input[attr] = scraped_data[attr]
-
-		# here because durration value from scraped movie is string where update preferrs an int need to cast to an int (seconds)
-		if scraped_data.duration:
-			if scraped_data.duration.count(':') == 0:
-				scraped_data.duration = f'00:00:{scraped_data.duration}'
-			if scraped_data.duration.count(':') == 1:
-				scraped_data.duration = f'00:{scraped_data.duration}'
-			h,m,s = scraped_data.duration.split(':')
-			durr = datetime.timedelta(hours=int(h),minutes=int(m),seconds=int(s)).total_seconds()
-			movie_update_input['duration'] = int(durr)
-
-		if scraped_data.studio.stored_id:
-			movie_update_input['studio_id'] = scraped_data.studio.stored_id
-
-		self.client.update_movie(movie_update_input)
+	def __update_movie_with_scrape_data(self, movie, scraped_movie):
+		movie_update = { 'id': movie.id }
+		movie_update.update( self.parse.get_movie_input(scraped_movie) )
+		self.client.update_movie(movie_update)
 
 	def __scrape_performers_with_fragment(self, performers, scraper_id):
 		return self.__scrape_with_fragment(
@@ -771,57 +611,10 @@ class ScrapeController:
 			self.client.scrape_performer_url,
 			self.__update_performer_with_scrape_data
 		)
-	def __update_performer_with_scrape_data(self, performer, scraped):
-		# Expecting to cast ScrapedPerformer to PerformerUpdateInput
-		# NOTE
-		# 	ScrapedPerformer.gender: String => PerformerUpdateInput.gender: GenderEnum
-		#   ScrapedPerformer.weight: String (kg?) => PerformerUpdateInput.weight: Int (kg)
-
-		update_data = {
-			'id': performer.id
-		}
-
-		common_attrabutes = [
-			'name',
-			'url',
-			'birthdate',
-			'ethnicity',
-			'country',
-			'eye_color',
-			'height',
-			'measurements',
-			'fake_tits',
-			'career_length',
-			'tattoos',
-			'piercings',
-			'aliases',
-			'twitter',
-			'instagram',
-			'image',
-			'details',
-			'death_date',
-			'hair_color'
-		]
-		for attr in common_attrabutes:
-			if scraped[attr]:
-				update_data[attr] = scraped[attr]
-
-		# cast String to Int, this assumes both values are the same unit and are just diffrent types
-		if scraped.weight:
-			update_data['weight'] = int(scraped.weight)
-
-		GENDER_ENUM = ["MALE","FEMALE","TRANSGENDER_MALE","TRANSGENDER_FEMALE", "INTERSEX", "NON_BINARY"]
-
-		if scraped.gender:
-			gender = scraped.gender.replace(' ', '_').upper()
-			if gender in GENDER_ENUM:
-				update_data['gender'] = gender
-			else:
-				log.warning(f'Could not map {scraped.gender} to a GenderEnum for performer {performer.id}')
-
-		self.client.update_performer(update_data)
-
-
+	def __update_performer_with_scrape_data(self, performer, scraped_performer):
+		performer_update = { 'id': performer.id }
+		performer_update.update( self.parse.get_movie_input(scraped_performer) )
+		self.client.update_performer(performer_update)
 
 	def __merge_tags(self, old_tag_ids, new_tag_ids):
 		merged_tags = set()
@@ -830,10 +623,6 @@ class ScrapeController:
 		if new_tag_ids:
 			merged_tags.update(new_tag_ids)
 		return list(merged_tags)
-
-# Capitalize each word in a string
-def caps_string(string, delim=" "):
-	return delim.join(x.capitalize() for x in string.split(delim))
 
 
 if __name__ == '__main__':
---
+++ scrape_parser.py
@@ -0,0 +1,159 @@
+import datetime
+from urllib.parse import urlparse
+
+import log
+import utils
+import config
+
+class ScrapeParser:
+
+  def __init__(self, client):
+    self.client = client
+
+  def __multi_item_list(self, __callback, items):
+    item_ids = [__callback(i) for i in items]
+    return [ item_id for item_id in item_ids if item_id ]
+
+  def get_common_atttrs(self, scrape_data, attr_list):
+    common_dict = {}
+    for attr in attr_list:
+      if scrape_data.get(attr):
+        common_dict[attr] = scrape_data[attr]
+    return common_dict
+
+  def get_studio_id(self, studio):
+    if studio.stored_id:
+      return studio.stored_id
+    elif config.create_missing_studios:
+      studio = {
+        'name': utils.caps_string(studio.name),
+        'url': '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(studio.url))
+      }
+      log.info(f'Creating missing studio {studio.get("name")}')
+      return self.client.create_studio(studio)
+
+  def get_tag_ids(self, tags):
+    return self.__multi_item_list(self.get_tag_id, tags)
+  def get_tag_id(self, tag):
+    if tag.stored_id:
+      return tag.stored_id
+    elif config.create_missing_tags and tag.name:
+      tag_name = utils.caps_string(tag.name)
+      log.info(f'Create missing tag: {tag_name}')
+      return self.client.create_tag({'name':tag_name})
+      
+  def get_performer_ids(self, performers):
+    return self.__multi_item_list(self.get_performer_id, performers)
+  def get_performer_id(self, performer):
+    if performer.stored_id:
+      return performer.stored_id
+    elif performer.name:
+      log.debug(f'Stash could not match {performer.name} doing custom match')
+      # scraper could not match performer, try re-matching or create if enabled
+      performer.name = performer.name.strip()
+      performer.name = utils.caps_string(performer.name)
+      
+      stash_performer = self.client.find_performer( {'name': performer.name } )
+      if stash_performer and stash_performer.get("id"):
+        log.debug(f'Custom match found performer')
+        return stash_performer.id
+
+      if config.create_missing_performers:
+        stash_performer = self.client.create_performer( self.get_performer_input(performer) )
+        if stash_performer and stash_performer.get("id"):
+          return stash_performer.id
+  def get_performer_input(self, performer):
+        # Expecting to cast ScrapedPerformer to PerformerCreate/UpdateInput 
+        # NOTE
+        # 	ScrapedPerformer.gender: String => PerformerCreateInput.gender: GenderEnum
+        #   ScrapedPerformer.weight: String (kg?) => PerformerCreateInput.weight: Int (kg)
+        performer_data = {}
+        common_attrabutes = [
+          'name',
+          'url',
+          'birthdate',
+          'ethnicity',
+          'country',
+          'eye_color',
+          'height',
+          'measurements',
+          'fake_tits',
+          'career_length',
+          'tattoos',
+          'piercings',
+          'aliases',
+          'twitter',
+          'instagram',
+          'image',
+          'details',
+          'death_date',
+          'hair_color'
+        ]
+        for attr in common_attrabutes:
+          if performer.get(attr):
+            performer_data[attr] = performer[attr]
+
+        # cast String to Int, this assumes both values are the same unit and are just diffrent types
+        if performer.weight:
+          performer_data['weight'] = int(performer.weight)
+
+        GENDER_ENUM = ["MALE","FEMALE","TRANSGENDER_MALE","TRANSGENDER_FEMALE", "INTERSEX", "NON_BINARY"]
+
+        if performer.gender:
+          gender = performer.gender.replace(' ', '_').upper()
+          if gender in GENDER_ENUM:
+            performer_data['gender'] = gender
+          else:
+            log.warning(f'Could not map {performer.gender} to a GenderEnum for performer {performer.id}')
+        
+        return performer_data
+
+  def get_movie_ids(self, movies):
+    return self.__multi_item_list(self.get_movie_id, movies)
+  def get_movie_id(self, movie):
+    if movie.stored_id:
+      return {'movie_id':movie.stored_id, 'scene_index':None}
+    elif config.create_missing_movies and movie.name:
+      log.info(f'Create missing movie: "{movie.name}"')
+      try:
+        stash_movie = self.client.create_movie(self.get_movie_input(movie))
+        return {'movie_id':stash_movie.id, 'scene_index':None}
+      except Exception as e:
+        log.error(f'movie create error')
+        log.error(str(e))
+  def get_movie_input(self, movie):
+    # NOTE
+    #  duration: String (HH:MM:SS) => duration: Int (Total Seconds)
+    #  studio: {ScrapedMovieStudio} => studio_id: ID
+
+    movie_data = {
+      'name': movie.name
+    }
+    common_attr = [
+      'name',
+      'aliases',
+      'date',
+      'rating',
+      'director',
+      'url',
+      'synopsis',
+      'front_image',
+      'back_image'
+    ]
+    movie_data.update( self.get_common_atttrs( movie, common_attr) )
+    
+    # here because durration value from scraped movie is string where update preferrs an int need to cast to an int (seconds)
+    if movie.duration:
+      if movie.duration.count(':') == 0:
+        movie.duration = f'00:00:{movie.duration}'
+      if movie.duration.count(':') == 1:
+        movie.duration = f'00:{movie.duration}'
+      h,m,s = movie.duration.split(':')
+      durr = datetime.timedelta(hours=int(h),minutes=int(m),seconds=int(s)).total_seconds()
+      movie_data['duration'] = int(durr)
+
+    if movie.studio:
+      movie_data['studio_id'] = self.get_studio_id(movie.studio)
+
+    movie_data = utils.clean_dict(movie_data)
+    return movie_data
